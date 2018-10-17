079afd2e72668a88817cbb6eb6970b3d7971bbbc
James Dix <42378279+JDix76@users.noreply.github.com>
2018-10-17 15:37:21 -0400

    Add files via upload

---
+++ Exercises/GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 1000);
+            int guess = 0;
+            int numberOfGuesses = 10;
+            bool correct = false;
+
+            Console.WriteLine("Let's play a game! Pick a number between 1 and 1000:");
+            Console.WriteLine();
+            Console.WriteLine("Please input your guess: ");
+
+            while (correct == false)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+                numberOfGuesses--;
+
+
+                if (guess != randomNumber && numberOfGuesses == 0)
+                {
+           
+                    Console.WriteLine("Ooops, I forgot to tell you that you only had 10 tries...hahaha..YOU LOSE!!");
+                    correct = true;
+                }
+
+                else if (guess == randomNumber)
+                {
+                    Console.WriteLine("You guess the right number...CONGRATS!!!");
+                    correct = true;
+                }
+
+                else if (guess > randomNumber)
+                {
+                    Console.WriteLine("Sorry! The number is lower than that.");
+                } 
+                else if (guess < randomNumber)
+                    Console.WriteLine("Nope! The number is higher than that.");
+            
+            }
+
+
+        }
+    }
+}
---
+++ Exercises/GuessingGameMachine/GuessingGameMachine/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameMachine
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play a game computer! Can you guess my number between 1 - 100");
+
+            int start = 1;
+            int end = 100;
+            int cpuGuess = (start + end) / 2;
+
+            Guess(cpuGuess, start, end);
+        }
+
+        private static void Guess(int cpuGuess, int start, int end)
+        {
+            Console.WriteLine("User input commands: higher, lower, and bingo");
+            Console.WriteLine($"My guess is: {cpuGuess}");
+            string answer = Console.ReadLine();
+
+            if (answer == "lower")
+            {
+                end = cpuGuess - 1;
+                cpuGuess = (end + start) / 2;
+                Guess(cpuGuess, start, end);
+            }
+            else if (answer == "higher")
+            {
+                start = cpuGuess + 1;
+                cpuGuess = (start + end) / 2;
+                Guess(cpuGuess, start, end);
+            }
+            else if (answer == "bingo")
+            {
+                Console.WriteLine("Congrats!");
+            }
+
+        }
+    }
+}
+
+    
+           
+
+    
+        
+    
+        
+    
+
